ab74c39a459e59c17c5669adaff5245b5d8d20df
yogeshwarrj <56185951+yogeshwarrj@users.noreply.github.com>
2019-10-30 23:49:04 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n1,i,p, n2, gcd;
+    int n1,i,n2, gcd;
     printf("Enter two integers: ");
     scanf("%d %d", &n1, &n2);
     for(i=1; i <= n1 && i <= n2; ++i)
